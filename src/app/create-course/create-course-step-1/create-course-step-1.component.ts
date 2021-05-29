0b6213ad1717bdc347b8e2d90d88d7241d3de3c5
Gagan <g.deepsingh1@gmail.com>
2021-05-29 20:45:49 +0530

    dropdown changes

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,7 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import { Observable } from 'rxjs';
 import { CoursesService } from '../../services/courses.service';
 import { courseTitleValidator } from '../../validators/course-title.validator';
+
+interface CourseCategory {
+  code: string;
+  description: string;
+}
 
 @Component({
   selector: 'create-course-step-1',
@@ -15,15 +21,19 @@ export class CreateCourseStep1Component implements OnInit {
       asyncValidators: [courseTitleValidator(this.courseService)],
       updateOn: 'blur'
     }],
+    category: ['BEGINNER', Validators.required],
     releasedAt: [new Date(), Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', Validators.minLength(5)]
   })
 
+  courseCategories$: Observable<CourseCategory[]>;
+
   constructor(private fb: FormBuilder, private courseService: CoursesService) {
   }
 
   ngOnInit() {
+    this.courseCategories$ = this.courseService.findCourseCategories();
   }
 
   get courseTitle() {
